05a2595548a5e0b12ede9334606d532546db0b1e
ereldebel <erel.debel@mail.huji.ac.il>
2022-05-31 21:42:30 +0300

    Initial commit

---
+++ preprocessing.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+import sklearn
+import plotly
+import tensorflow
+import xgboost as xgb
+import glmnet_python
+
+
+if __name__ == '__main__':
+	print("oh yeah")
