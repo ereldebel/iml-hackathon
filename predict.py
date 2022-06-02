824e5e4d51e0bfcdef51d1d5dd756866dab58d5a
ereldebel <erel.debel@mail.huji.ac.il>
2022-06-02 19:20:48 +0300

    updated predict

--- predict.py
+++ predict.py
@@ -3,15 +3,20 @@ from sklearn.ensemble import ExtraTreesClassifier
 from sklearn.model_selection import train_test_split
 from process_features import process_features_single, process_features_combined
 from sklearn.metrics import f1_score, confusion_matrix
+import matplotlib.pyplot as plt
+from sklearn.metrics import ConfusionMatrixDisplay
+from sklearn.model_selection import train_test_split
 
 
 def train_and_predict(X: pd.DataFrame, y: pd.DataFrame):
-	y = y["linqmap_type_label"]
+	y = y["linqmap_subtype_label"]
 	X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=2 / 3)
 	model = ExtraTreesClassifier().fit(X_train, y_train)
-	y_pred = model.fit(X_train)
-	print(f1_score(y_train, y_pred))
-	print(confusion_matrix(y_train, y_pred, labels=y_train.unique()))
+	y_pred = model.predict(X_test)
+	print(f1_score(y_test, y_pred, average="macro"))
+	ConfusionMatrixDisplay.from_estimator(
+		model, X_test, y_test)
+	plt.show()
 
 
 if __name__ == '__main__':
