69f0ce33d502a946c60e0d2475a2639f0742d849
ereldebel <erel.debel@mail.huji.ac.il>
2022-06-03 02:01:13 +0300

    reprocessed data

--- predict.py
+++ predict.py
@@ -2,15 +2,18 @@ import pandas as pd
 from unified_estimator import UnifiedEstimator
 
 if __name__ == '__main__':
-	train_X_path = "datasets/train_set_X_all_cities.csv"
-	train_X = pd.read_csv(train_X_path)
-	train_y_path = "datasets/train_set_y_all_cities.csv"
-	train_y = pd.read_csv(train_y_path)
+	for data_name in ("TLV","all_cities"):
+		print (data_name)
 
-	test_X_path = "datasets/test_set_X_all_cities.csv"
-	test_X = pd.read_csv(test_X_path)
-	test_y_path = "datasets/test_set_y_all_cities.csv"
-	test_y = pd.read_csv(test_y_path)
+		train_X_path = f"datasets/train_set_X_{data_name}.csv"
+		train_X = pd.read_csv(train_X_path)
+		train_y_path = f"datasets/train_set_y_{data_name}.csv"
+		train_y = pd.read_csv(train_y_path)
 
-	model = UnifiedEstimator().fit(test_X, test_y)
-	model.loss(train_X, train_y)
+		test_X_path = f"datasets/test_set_X_{data_name}.csv"
+		test_X = pd.read_csv(test_X_path)
+		test_y_path = f"datasets/test_set_y_{data_name}.csv"
+		test_y = pd.read_csv(test_y_path)
+
+		model = UnifiedEstimator().fit(train_X, train_y)
+		model.loss(test_X, test_y)
