8b45e1c87f826a02b78a2028d3f299939f2fb460
ereldebel <erel.debel@mail.huji.ac.il>
2022-06-02 23:14:02 +0300

    added CV

--- unified_estimator.py
+++ unified_estimator.py
@@ -10,17 +10,16 @@ import matplotlib.pyplot as plt
 from sklearn.metrics import ConfusionMatrixDisplay
 from sklearn.model_selection import train_test_split
 
-from typing import NoReturn
+from typing import NoReturn,Tuple
 
 
 class UnifiedEstimator:
 	def __init__(self, type_model=ExtraTreesClassifier(),
-				 subtype_model=ExtraTreesClassifier(),
-				 x_model = GradientBoostingRegressor(),
-				 y_model = GradientBoostingRegressor()):
-
+	             subtype_model=ExtraTreesClassifier(),
+	             x_model=GradientBoostingRegressor(),
+	             y_model=GradientBoostingRegressor()):
 		self._type_model = type_model
-		self._subtype_model =subtype_model
+		self._subtype_model = subtype_model
 		self._x_model = x_model
 		self._y_model = y_model
 		self._fitted = False
@@ -42,7 +41,7 @@ class UnifiedEstimator:
 		y_pred = self._y_model.predict(X).reshape(-1, 1)
 		return np.column_stack([x_pred, y_pred, type_pred, subtype_pred])
 
-	def loss(self, X: pd.DataFrame, y_true: pd.DataFrame) -> np.ndarray:
+	def loss(self, X: pd.DataFrame, y_true: pd.DataFrame) -> Tuple[float]:
 		y_pred = self.predict(X)
 		y_pred_x = y_pred[:, 0]
 		y_pred_y = y_pred[:, 1]
@@ -54,18 +53,21 @@ class UnifiedEstimator:
 		y_true_type = y_true["linqmap_type_label"].values
 		y_true_subtype = y_true["linqmap_subtype_label"].values
 
-		print("type f1 macro:",
-		      f1_score(y_true_type, y_pred_type, average="macro"))
+		type_loss = f1_score(y_true_type, y_pred_type, average="macro")
+		print("type f1 macro:", type_loss)
 		ConfusionMatrixDisplay.from_estimator(self._type_model, X, y_true_type)
 		plt.show()
-		print("subtype f1 macro:",
-		      f1_score(y_true_subtype, y_pred_subtype, average="macro"))
+		subtype_loss = f1_score(y_true_subtype, y_pred_subtype,
+		                        average="macro")
+		print("subtype f1 macro:", subtype_loss)
 		ConfusionMatrixDisplay.from_estimator(self._subtype_model, X,
 		                                      y_true_subtype)
 		plt.show()
-		print("x MSE: ", mean_squared_error(y_true_x, y_pred_x))
-		print("y MSE: ", mean_squared_error(y_true_y, y_pred_y))
-		return y_pred
+		x_loss = mean_squared_error(y_true_x, y_pred_x)
+		print("x MSE: ", x_loss)
+		y_loss = mean_squared_error(y_true_y, y_pred_y)
+		print("y MSE: ", y_loss)
+		return type_loss, subtype_loss, x_loss, y_loss
 
 	def x_loss(self, y_pred: pd.DataFrame, y_true: pd.DataFrame) -> np.ndarray:
 		y_pred_x = y_pred[:, 0]
