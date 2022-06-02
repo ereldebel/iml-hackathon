f1042ebd53861b2d15786926c43efe6cecb20d98
ereldebel <erel.debel@mail.huji.ac.il>
2022-06-02 20:22:56 +0300

    added unified estimator

--- predict.py
+++ predict.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from sklearn.ensemble import ExtraTreesClassifier
 from sklearn.model_selection import train_test_split
+
+from unified_estimator import UnifiedEstimator
 from process_features import process_features_single, process_features_combined
 from sklearn.metrics import f1_score, confusion_matrix
 import matplotlib.pyplot as plt
@@ -9,14 +11,9 @@ from sklearn.model_selection import train_test_split
 
 
 def train_and_predict(X: pd.DataFrame, y: pd.DataFrame):
-	y = y["linqmap_subtype_label"]
 	X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=2 / 3)
-	model = ExtraTreesClassifier().fit(X_train, y_train)
-	y_pred = model.predict(X_test)
-	print(f1_score(y_test, y_pred, average="macro"))
-	ConfusionMatrixDisplay.from_estimator(
-		model, X_test, y_test)
-	plt.show()
+	model = UnifiedEstimator().fit(X_train, y_train)
+	model.loss(X_test, y_test)
 
 
 if __name__ == '__main__':
